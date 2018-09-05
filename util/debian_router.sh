b589c057b4cbeac5c23e7e585b576af762e9bc04
Dirk Morris <dmorris@untangle.com>
2018-09-05 03:38:54 +0000

    Make test self sufficient by creating its own table

--- util/debian_router.sh
+++ util/debian_router.sh
@@ -5,18 +5,16 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 modprobe nft_chain_nat_ipv4
 modprobe nft_chain_nat_ipv6
 
-nft add table ip postrouting
-nft add table ip prerouting
+nft flush table ip nat 2>/dev/null || true
+nft add table ip nat
 
-nft add chain ip postrouting postrouting-nat "{ type nat hook postrouting priority 100 ; }"
-nft add chain ip prerouting  prerouting-nat "{ type nat hook prerouting priority -100 ; }"
+nft add chain ip nat  postrouting-nat "{ type nat hook postrouting priority 100 ; }"
+nft add chain ip nat  prerouting-nat  "{ type nat hook prerouting priority -100 ; }"
 
-nft add rule ip postrouting postrouting-nat oifname lo accept
-nft add rule ip postrouting postrouting-nat iifname lo accept
-nft add rule ip postrouting postrouting-nat counter masquerade
+nft add rule ip nat postrouting-nat oifname lo accept
+nft add rule ip nat postrouting-nat iifname lo accept
+nft add rule ip nat postrouting-nat counter masquerade
 
-nft add table inet forward
-nft add chain inet forward forward-filter "{ type filter hook forward priority 0 ; }"
 
 
 
