f2c2f1a387b285716835c0af1f7507a14183724b
mahotz <mahotz@untangle.com>
2018-11-08 13:35:44 -0700

    Added script with examples for calling the new restd api functions for warehouse playback and traffic bypass.

---
+++ tests/traffic.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#
+# Examples of calling the restd functions for traffic playback and control
+#
+
+# Turn on the live traffic bypass flag
+RESULT="`curl -X POST -s -o - -H 'Content-Type: application/json; charset=utf-8' -d '{"bypass":"TRUE"}' 'http://localhost:8080/api/control/traffic'`"
+echo $RESULT
+
+# Playback a traffic capture a 2x speed
+RESULT="`curl -X POST -s -o - -H 'Content-Type: application/json; charset=utf-8' -d '{"filename":"/tmp/warehouse.cap","speed":"2"}' 'http://localhost:8080/api/warehouse/playback'`"
+echo $RESULT
+
+# Turn off the live traffic bypass flag
+RESULT="`curl -X POST -s -o - -H 'Content-Type: application/json; charset=utf-8' -d '{"bypass":"FALSE"}' 'http://localhost:8080/api/control/traffic'`"
+echo $RESULT
