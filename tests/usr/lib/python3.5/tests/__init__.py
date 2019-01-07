ac07653c0cc379dbe72ab1a0b8c693c2e65b2375
Dirk Morris <dmorris@untangle.com>
2019-01-07 01:28:38 +0000

    reconcile runtests with ngfw

--- tests/usr/lib/python3.5/tests/__init__.py
+++ tests/usr/lib/python3.5/tests/__init__.py
@@ -2,12 +2,3 @@
 import tests.remote_control
 import tests.global_functions
 import tests.test_registry
-
-from tests.environment_tests import TestEnvironmentTests
-from tests.settings_tests import SettingsTests
-from tests.nftables_tests import NftablesTests
-from tests.geoip_tests import GeoipTests
-from tests.playback_tests import PlaybackTests
-from tests.reports_tests import ReportsTests
-from tests.dict_tests import DictTests
-
