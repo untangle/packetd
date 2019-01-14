ffb5dfc565c73fa45a77744af60d043266dedb45
mahotz <mahotz@untangle.com>
2019-01-14 11:10:14 -0700

    Disable IPv6 NFCT_Q_DUMP call for now

--- services/kernel/conntrack.c
+++ services/kernel/conntrack.c
@@ -204,7 +204,9 @@ void conntrack_dump(void)
 	ret = nfct_send(nfcth,NFCT_Q_DUMP,&family);
 	if (ret < 0) logmessage(LOG_WARNING,logsrc,"nfct_send(AF_INET) result = %d\n",ret);
 
+/* FIXME - figure out why this breaks everything
 	family = AF_INET6;
 	ret = nfct_send(nfcth,NFCT_Q_DUMP,&family);
 	if (ret < 0) logmessage(LOG_WARNING,logsrc,"nfct_send(AF_INET6) result = %d\n",ret);
+*/
 }
