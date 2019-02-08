835bc233b6c300a6add8856508e3ccb54190e04b
Dirk Morris <dmorris@untangle.com>
2019-02-08 02:09:40 +0000

    fix sessions test

--- tests/usr/lib/python3.5/tests/status_tests.py
+++ tests/usr/lib/python3.5/tests/status_tests.py
@@ -5,12 +5,11 @@
 import subprocess
 import unittest
 import json
-import sys
 import runtests.test_registry as test_registry
 
-def get_status(str, seperator="/"):
+def get_status(call):
     """Gets the specified status and returns the JSON Object or None if there is any error"""
-    result = subprocess.run('curl -m 5 -X GET -s -o - -H "Content-Type: application/json; charset=utf-8" "http://localhost/api/status/%s"' % str, shell=True, stdout=subprocess.PIPE)
+    result = subprocess.run('curl -m 5 -X GET -s -o - -H "Content-Type: application/json; charset=utf-8" "http://localhost/api/status/%s"' % call, shell=True, stdout=subprocess.PIPE)
     if result.returncode != 0:
         return None
     else:
@@ -27,7 +26,7 @@ class StatusTests(unittest.TestCase):
     def initial_setup(self):
         """initial_setup unittest method"""
         pass
-        
+
     def setUp(self):
         print()
 
@@ -52,7 +51,7 @@ class StatusTests(unittest.TestCase):
         if len(sessions) > 0:
             assert sessions[0].get("client_address") != None
             assert sessions[0].get("server_address") != None
-            assert sessions[0].get("protocol") != None
+            assert sessions[0].get("family") != None
             assert sessions[0].get("ip_protocol") != None
 
     def test_003_get_status_hardware(self):
@@ -61,9 +60,9 @@ class StatusTests(unittest.TestCase):
         assert hardware_status.get("cpuinfo") != None
         assert hardware_status.get("cpuinfo").get("processors") != None
         assert len(hardware_status.get("cpuinfo").get("processors")) > 0
-            
+
     def final_tear_down(self):
         """final_tear_down unittest method"""
         pass
-        
+
 test_registry.register_module("status", StatusTests)
