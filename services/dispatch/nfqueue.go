9d752f1092db4cdac4e662f4d84bb9726e1d272f
Dirk Morris <dmorris@untangle.com>
2019-02-09 00:15:33 +0000

    Move conntrack check till after session

--- services/dispatch/nfqueue.go
+++ services/dispatch/nfqueue.go
@@ -128,18 +128,6 @@ func nfqueueCallback(ctid uint32, family uint32, packet gopacket.Packet, packetL
 
 	session := findSession(ctid)
 
-	// Sanity check - if this is a new session we should not have an existing conntrack entry (yet)
-	if newSession {
-		conntrack, _ := findConntrack(ctid)
-		if conntrack != nil {
-			logger.Warn("Found existing conntrack for new session:\n")
-			logger.Warn("New Session        : %v\n", mess.MsgTuple)
-			logger.Warn("Existing Conntrack : %v\n", conntrack.ClientSideTuple)
-		}
-		removeConntrack(ctid)
-	}
-	// lookupConntrack(entry)
-
 	if session == nil {
 		if !newSession {
 			// If we did not find the session in the session table, and this isn't a new packet
@@ -196,6 +184,17 @@ func nfqueueCallback(ctid uint32, family uint32, packet gopacket.Packet, packetL
 	}
 
 	mess.Session = session
+
+	// Sanity check - if this is a new session we should not have an existing conntrack entry (yet)
+	if newSession {
+		conntrack, _ := findConntrack(ctid)
+		if conntrack != nil {
+			logger.Warn("Found existing conntrack for new session:\n")
+			logger.Warn("New Session        : %v\n", mess.MsgTuple)
+			logger.Warn("Existing Conntrack : %v\n", conntrack.ClientSideTuple)
+		}
+		removeConntrack(ctid)
+	}
 
 	if mess.MsgTuple.ClientAddress.Equal(session.ClientSideTuple.ClientAddress) {
 		mess.ClientToServer = true
