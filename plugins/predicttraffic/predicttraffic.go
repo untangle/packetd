4a6b4395f2aaed1015f6f079e014a7069ab5c67b
John Sommerville <jsommerville@untangle.com>
2019-08-06 15:53:01 -0600

    Adding dict classification and report event log

--- plugins/predicttraffic/predicttraffic.go
+++ plugins/predicttraffic/predicttraffic.go
@@ -1,9 +1,11 @@
 package predicttraffic
 
 import (
+	"github.com/untangle/packetd/services/dict"
 	"github.com/untangle/packetd/services/dispatch"
 	"github.com/untangle/packetd/services/logger"
 	"github.com/untangle/packetd/services/predicttrafficsvc"
+	"github.com/untangle/packetd/services/reports"
 )
 
 const pluginName = "predicttraffic"
@@ -36,7 +38,44 @@ func PluginNfqueueHandler(mess dispatch.NfqueueMessage, ctid uint32, newSession 
 	//Tell service to cache and categorize this ip request
 	logger.Debug("Running traffic classification on request: Client address: %s, client port: %d, Server address: %s, Server port: %d, Protocol: %d,  \n", mess.MsgTuple.ClientAddress, mess.MsgTuple.ClientPort, mess.MsgTuple.ServerAddress, mess.MsgTuple.ServerPort, mess.MsgTuple.Protocol)
 
-	predicttrafficsvc.GetTrafficClassification(ctid, mess.MsgTuple.ServerAddress, mess.MsgTuple.ServerPort, mess.MsgTuple.Protocol)
+	var trafficInfo = predicttrafficsvc.GetTrafficClassification(mess.MsgTuple.ServerAddress, mess.MsgTuple.ServerPort, mess.MsgTuple.Protocol)
+
+	if trafficInfo != nil {
+		addPredictionToDict(ctid, trafficInfo)
+		addPredictionToReport(mess, trafficInfo)
+	}
 
 	return result
+}
+
+// addPredictionToDict will take a ClassifiedTraffic pointer and send the data to dict
+func addPredictionToDict(ctid uint32, currentTraffic *predicttrafficsvc.ClassifiedTraffic) {
+	logger.Debug("Sending prediction info to dict with ctid: %d\n", ctid)
+	dict.AddSessionEntry(ctid, "application_predicted_name", currentTraffic.Application)
+	dict.AddSessionEntry(ctid, "application_predicted_confidence", roundConfidence(currentTraffic.Confidence))
+	dict.AddSessionEntry(ctid, "application_predicted_protochain", currentTraffic.ProtocolChain)
+}
+
+// addPredictionToReport will take a ClassifiedTraffic pointer and send the data into the reports sqlite database, under the sessions table
+func addPredictionToReport(mess dispatch.NfqueueMessage, currentTraffic *predicttrafficsvc.ClassifiedTraffic) {
+	logger.Debug("Sending prediction info to sessions table: %d\n", mess.Session.GetSessionID())
+
+	columns := map[string]interface{}{
+		"session_id": mess.Session.GetSessionID(),
+	}
+
+	modifiedColumns := make(map[string]interface{})
+	modifiedColumns["application_predicted_name"] = currentTraffic.Application
+	modifiedColumns["application_predicted_confidence"] = roundConfidence(currentTraffic.Confidence)
+	modifiedColumns["application_predicted_protochain"] = currentTraffic.ProtocolChain
+
+	reports.LogEvent(reports.CreateEvent("session_predict_traffic", "sessions", 2, columns, modifiedColumns))
+}
+
+// round confidence converts the confidence from a float32 into a uint8, with very basic logic to round up or down
+func roundConfidence(conf float32) uint8 {
+	if conf < 0 {
+		return uint8(conf - 0.5)
+	}
+	return uint8(conf + 0.5)
 }
