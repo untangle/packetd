5add66474ea30a225a2ac749be9ac3bada4b56fc
John Sommerville <jsommerville@untangle.com>
2019-08-28 11:01:42 -0600

    change API types to uint8

--- services/predicttrafficsvc/predicttrafficsvc.go
+++ services/predicttrafficsvc/predicttrafficsvc.go
@@ -28,8 +28,8 @@ type ClassifiedTraffic struct {
 	Name         string  `json:"ApplicationName"`
 	Confidence   float32 `json:"Confidence"`
 	ProtoChain   string  `json:"Protocolchain"`
-	Productivity string  `json:"ApplicationProductivity"`
-	Risk         string  `json:"ApplicationRisk"`
+	Productivity uint8   `json:"ApplicationProductivity"`
+	Risk         uint8   `json:"ApplicationRisk"`
 	Category     string  `json:"ApplicationCategory"`
 }
 
@@ -40,7 +40,7 @@ type CachedTrafficItem struct {
 }
 
 // unknownTrafficItem is a pointer for unknown traffic
-var unknownTrafficItem = &ClassifiedTraffic{ID: "Unknown", Name: "Unknown", Confidence: 0, ProtoChain: "Unknown", Productivity: "Unknown", Risk: "Unknown", Category: "Unknown"}
+var unknownTrafficItem = &ClassifiedTraffic{ID: "Unknown", Name: "Unknown", Confidence: 0, ProtoChain: "Unknown", Productivity: 0, Risk: 0, Category: "Unknown"}
 
 // classifiedTrafficCache is a map of ClassifiedTraffic pointer structs
 var classifiedTrafficCache map[string]*CachedTrafficItem
@@ -106,7 +106,6 @@ func GetTrafficClassification(ipAdd net.IP, port uint16, protoID uint8) *Classif
 func sendClassifyRequest(ipAdd net.IP, port uint16, protoID uint8) *ClassifiedTraffic {
 
 	var trafficResponse *ClassifiedTraffic
-	client := &http.Client{}
 
 	requestURL := formRequestURL(ipAdd, port, protoID)
 
@@ -128,7 +127,7 @@ func sendClassifyRequest(ipAdd net.IP, port uint16, protoID uint8) *ClassifiedTr
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("AuthRequest", authRequestKey)
 
-	client = &http.Client{Transport: tr}
+	client := &http.Client{Transport: tr}
 	resp, err := client.Do(req)
 
 	if err != nil {
