0fddc301f34b2256ea22c49aa26e6c04caec98e1
John Sommerville <jsommerville@untangle.com>
2020-10-06 15:30:59 -0600

    vsbuild: add some flags for target/port/localmuslbuild to allow for passing tasks with custom ports or for forcing a build using the local musl docker build

--- .vscode/vsbuild.sh
+++ .vscode/vsbuild.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 ##
-## Build mfw admin UI
+## Build mfw packetd
 ##
 TARGET=$1
+PORT=22
+LOCAL_MUSL_BUILD=false
 
-docker-compose -f build/docker-compose.build.yml up --build musl
-ssh root@$TARGET "/etc/init.d/packetd stop"; 
+while getopts 't:p:m:' flag; do
+    case "${flag}" in
+        t) TARGET=${OPTARG} ;;
+        p) PORT=${OPTARG} ;;
+        m) LOCAL_MUSL_BUILD=${OPTARG} ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ LOCAL_MUSL_BUILD ]
+then
+    docker-compose -f build/docker-compose.build.yml up --build musl-local
+else
+    docker-compose -f build/docker-compose.build.yml up --build musl
+fi
+
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p $PORT root@$TARGET "/etc/init.d/packetd stop"; 
 sleep 5
-scp ./cmd/packetd/packetd root@$TARGET:/usr/bin/; 
-ssh root@$TARGET "/etc/init.d/packetd start"
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P $PORT ./cmd/packetd/packetd root@$TARGET:/usr/bin/; 
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p $PORT root@$TARGET "/etc/init.d/packetd start"
 
